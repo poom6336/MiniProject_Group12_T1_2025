1a8d4db205eacc96d2c9854054f75459a2e6071e
poom6336 <54231004+poom6336@users.noreply.github.com>
2026-03-12 21:14:34 +0700

    V.0.1.0
    Can add course and undo but unable to add the same courses which have diff prof and unable to use process function atm.

---
+++ group12_T1.py
@@ -0,0 +1,70 @@
+import csv
+
+storage = []
+max_undo = 3
+dataName = ['CourseCode','Name','Type','Credit','Semester','Lecturer']
+mainCSV = 'MiniProj\CprE_Subject.csv'
+newCSV = 'MiniProj\course_data.csv'
+
+def undo(fName):
+    if storage:
+        stored_data = storage.pop()
+
+        with open(fName, 'w', newline='') as dataf:
+            print("REVERTED: ",)
+            writer = csv.DictWriter(dataf, dataName)
+            writer.writeheader()
+            writer.writerows(stored_data)
+
+def read_csv(fName):
+    with open(fName,'r') as f:
+        return list(csv.DictReader(f))
+        
+def write_csv(fName, data):
+    global storage
+
+    current_data = read_csv(fName)
+    storage.append(current_data)
+
+    if len(storage) > max_undo:
+        storage.pop(0)
+
+    with open(fName, 'w', newline='') as dataf:
+        writer = csv.DictWriter(dataf, dataName)
+        writer.writeheader()
+        writer.writerows(data)
+
+def find_csv(fName,find):
+    with open(fName, "r") as f:
+        reader = csv.DictReader(f)
+
+        for row in reader:
+            if row["CourseCode"] == find:
+                return row
+            
+    return None
+
+
+with open(newCSV, 'w', newline='') as coursef:
+    writer = csv.DictWriter(coursef, dataName)
+
+    writer.writeheader()
+
+while True:
+    usInput = input("Input: ")
+
+    if "add" in usInput:
+        tempSplit = usInput.split()
+        usCourse = tempSplit[1]
+        fillData = find_csv(mainCSV, usCourse)
+        
+        if fillData:
+            current = read_csv(newCSV)
+            current.append(fillData)
+            write_csv(newCSV, current)
+            print("Added", usCourse)
+        else:
+            print("Course not found")
+
+    elif "undo" in usInput:
+        undo(newCSV)
